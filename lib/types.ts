60fb74c033e2718c6cd448d5347e50878620f6d2
Kai Black <136423351+purelyKai@users.noreply.github.com>
2025-01-10 16:19:38 -0800

    Include UserAccount type

--- lib/types.ts
+++ lib/types.ts
@@ -20,6 +20,24 @@ export interface Plan {
     terms: Term[];
 }
 
+/* To be adjusted based on future user preference options */
+export interface UserPreferences {
+    likes_outdoors: number;
+}
+
+export interface UserAccount {
+    id: number;
+    email: string;
+    password: string;
+    first_name: string;
+    last_name: string;
+    majors: string[];
+    concentrations: string[];
+    minors: string[];
+    user_preferences: UserPreferences;
+    plans: Plan[];
+}
+
 export type Params = Promise<{ planId: string }>;
 
 export type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
