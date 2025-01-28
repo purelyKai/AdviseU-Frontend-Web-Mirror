a93eb0acbaf5ffaf8bbaaa699aee80ec856fe447
Kai Black <kaibblack03+github@gmail.com>
2025-01-27 17:31:57 -0800

    MongoDB auth integration

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import client from './lib/mongodb';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+    adapter: MongoDBAdapter(client),
     providers: [
         Google({
             clientId: process.env.AUTH_GOOGLE_ID!,
